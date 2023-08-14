14e42aeb29a50c550788c428af6ee1c282de4927
Andy00 <multi.andres.martinez@gmail.com>
2023-08-14 10:57:08 -0600

    :bento: Alias en pages
    se usan alias en todos los archivos del folder page y se mejora la pagina NotFoud

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import '@styles/NotFound.scss';
 
 const NotFound = () => {
 	return (
 		<div>
-			No encontrado, error 404.
+			<h1 className='title-404'>404</h1>
+			<p className="text-404">Oops! Pagina no encontrada.</p>
+			<a className="button-404" href="./">Regrese a la página inicial, es mejor.</a>
 		</div>
 	);
 }
