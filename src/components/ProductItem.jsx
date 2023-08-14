c59502d1905a41484642e4657700ac5b9d9f983c
Andy00 <multi.andres.martinez@gmail.com>
2023-08-13 21:11:49 -0600

    :bento: Alias en los componentes
    se usaan alias en todos los archivos dentro de folder components

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../styles/ProductItem.scss';
+import '@styles/ProductItem.scss';
+import icon from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
 	return (
@@ -11,7 +12,7 @@ const ProductItem = () => {
 					<p>Bike</p>
 				</div>
 				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+					<img src={icon} alt="" />
 				</figure>
 			</div>
 		</div>
