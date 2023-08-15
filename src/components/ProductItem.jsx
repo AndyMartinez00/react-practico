9f078d070e0ab8623f90ddc7252554bfa4fbd989
Andy00 <multi.andres.martinez@gmail.com>
2023-08-14 20:48:16 -0600

    :sparkles: Usestates
    usando usestates

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '@styles/ProductItem.scss';
 import icon from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+	/*manejando hooks estado o ciclo de vida*/
+	/*dos valores 1-estado 2-funcion modificadora ,  */
+	const [cart, setCart] = useState('Hi5'); /*string,boleano,objeto,array*/
+	/*evento que desencadena el mejo de un elemento dentro del estado  */
+	const handleClick =()=>{
+		setCart('Hi5 World');
+	}
+
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -11,9 +19,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleClick} >
 					<img src={icon} alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
