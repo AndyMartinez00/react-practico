ed2c71f4ffac21e68ce2402e78b538c3e41fff76
Andy00 <multi.andres.martinez@gmail.com>
2023-08-16 14:53:55 -0600

    :sparkles: Usando useEffect y consumo de APIs
    se consume api usando axios y @babel/plugins-transform-runtime

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products';
+/* useEfecct permite hacer llammado a la api y transmitirlo anuestro componente */
 
 const ProductList = () => {
+	/* llamando la informacion useState*/
+	const [products, setProducts] = useState([]);	
+	/*useEffect recibe 2 elementos una funcion anonima, un arreglo que escucha los cambios*/
+	useEffect(async () => {
+		const response = await axios(API);
+		setProducts(response.data);
+	}, [])
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+			{products.map(product => (
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
