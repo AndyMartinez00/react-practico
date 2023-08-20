6f26395f6fd00dd5b2ab3e97e5d1105afaec642c
Andy00 <multi.andres.martinez@gmail.com>
2023-08-20 10:47:51 -0600

    :hammer: Usando useRef y formulario
    genereando referencia de fomulario usando useRef

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React,{ useRef } from 'react';
 import '@styles/Login.scss';
 import logo from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
+	const form = useRef(null);
+
+	const handleSubmit = (event) => {
+		
+		event.preventDefault();
+		const formData = new FormData(form.current);
+		//manera1
+		/*
+		const data = {
+			usename: formData.get('email'),
+			password: formData.get('password')
+		}*/
+		//manera2
+		const data = Object.fromEntries(formData);
+		
+		console.log(data);
+	}
+
 	return (
 		<div className="Login">
 			<div className="Login-container">
 				<img src={logo} alt="logo" className="logo" />
-				<form action="/" className="form">
+				<form action="/" className="form"  ref={form} >
 					<label htmlFor="email" className="label">Email address</label>
-					<input type="text" id="email" placeholder="platzi@example.cm" className="input input-email" />
+					<input type="text" id="email" name="email" placeholder="platzi@example.cm" className="input input-email" required />
 					<label htmlFor="password" className="label">Password</label>
-					<input type="password" id="password" placeholder="*********" className="input input-password" />
-					<input type="submit" value="Log in" className="primary-button login-button" />
+					<input type="password" id="password" name="password" placeholder="*********" className="input input-password" required />
+					{/*<input type="submit" value="Log in" className="primary-button login-button" onClick={handleSubmit} />*/}
+					<button className="primary-button login-button" onClick={handleSubmit} >Log in</button>
 					<a href="/">Forgot my password</a>
 				</form>
 				<button className="secondary-button signup-button">Sign up</button>
